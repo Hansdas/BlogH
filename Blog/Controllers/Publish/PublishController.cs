9290e1040da86f9d407e90bac92f77708daf3d49
3248996258 <3248996258@qq.com>
2019-05-21 22:49:51 +0800

    2019-5-21 22:49

---
+++ Blog/Controllers/Publish/PublishController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers.Add
+{
+    public class PublishController : Controller
+    {
+        public IActionResult Publish()
+        {
+            return View();
+        }
+    }
+}
--- Blog/wwwroot/js/layout.js
+++ Blog/wwwroot/js/layout.js
@@ -1,26 +1,26 @@
 
 $(function () {
-    //导航栏点击变色
-    $(".nav a").click(function () {
-        $(".nav a").each(function (index) {
-            $(this).removeClass("active");
-        })
-        $(this).addClass("active");
-        $("#console").attr("src", "../Whisper/Index")
-    });
-    //iframe自适应内容高度
-    $("#console").each(function (index) {
-        var that = $(this);
-        (function () {
-            setInterval(function () {
-                setIframeHeight(that[0])
-            }, 200)
-        })(that)
-    });
-    $(".layui-nav-item a").mouseover(function(){
-        $(this).attr("style","color:black !important");
-   })
-})
+	//导航栏点击变色
+	$(".nav a").click(function () {
+		$(".nav a").each(function (index) {
+			$(this).removeClass("active");
+		});
+		$(this).addClass("active");
+		$("#console").attr("src", "../Whisper/Index");
+	});
+	//iframe自适应内容高度
+	$("#console").each(function (index) {
+		var that = $(this);
+		(function () {
+			setInterval(function () {
+				setIframeHeight(that[0]);
+			}, 200);
+		})(that);
+	});
+	//$(".layui-nav-item a").mouseover(function () {
+	//	$(this).attr("style", "color:black !important");
+	//});
+});
 function setIframeHeight(iframe) {
     if (iframe) {
         var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
@@ -28,4 +28,4 @@ function setIframeHeight(iframe) {
             iframe.height = iframeWin.document.body.scrollHeight;
         }
     }
-};
+}
